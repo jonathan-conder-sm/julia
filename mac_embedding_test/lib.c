841fc15822d54faf47c4f51807f612ab100612a5
Jonathan Conder <jonathan.conder@soulmachines.com>
2022-10-06 09:51:08 +1300

    Add mac embedding test

---
+++ mac_embedding_test/lib.c
@@ -0,0 +1,9 @@
+#include <julia.h>
+JULIA_DEFINE_FAST_TLS
+
+int load_julia(void) {
+    jl_init_with_image(JULIA_BINDIR, NULL);
+    jl_eval_string("println(sqrt(2.0))");
+    jl_atexit_hook(0);
+    return 0;
+}
